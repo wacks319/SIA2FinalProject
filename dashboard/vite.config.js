09e82ece77c938e6beaf9ee8bfad44835ab3300d
kerty <briikzdev@gmail.com>
2024-12-07 14:33:45 +0800

    updated vite

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
-    port: '3000',
+    port: 3000,  // Make sure it's a number
   },
-  base: '/SIA2FinalProject-2/', // Set this to your repo name
+  base: '/', // Set this to '/' if deploying to the root on Vercel
 });
