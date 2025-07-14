5ebada3b3b7b8e9a7a7257541bb5c91c9c4cd6ef
wakin sanchez <hed-jsanchez@smu.edu.ph>
2025-07-14 10:54:31 +0800

    Test

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const path = require('path');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const app = express();
+//test
 
 const PORT = process.env.PORT || 3004;
 const HOST = 'localhost'
